99674816dd7f5d2ac2d381bc9b2cfa318f03fbde
Bashitha <bashipbr@gmail.com>
2023-06-13 20:03:27 -0400

    Docker Image

---
+++ src/test/java/com/ProductService/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ProductService.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+@EnableEurekaServer
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
